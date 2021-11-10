0340a6013b8b3a38c097a557f0775054179a2ba5
Dhaanish K <freinds528@gmail.com>
2021-11-10 17:51:07 -0500

    updated main and tetromino to handle move func and

--- src/Tetromino.m
+++ src/Tetromino.m
@@ -1,4 +1,4 @@
-classdef Tetromino
+classdef Tetromino < handle
     %TETROMINO Summary of this class goes here
     %   Detailed explanation goes here
     
@@ -16,50 +16,98 @@ classdef Tetromino
             %TETRAMINO Construct an instance of this class
             %   Detailed explanation goes here
             
-            obj.type = randi(6);
+            obj.type = randi(7);
+            %obj.type = 1;
             
             switch obj.type
                 case 1 %Line Piece
                     obj.color = 2;
                     obj.locations = [[1,4],[1,5],[1,6],[1,7]];
+                    
                     obj.left = 4;
                     obj.right = 7;
+                    obj.bottom = 1;
+ 
                 case 2 %Square Piece
                     obj.color = 3;
                     obj.locations = [[1,5],[1,6],[2,5],[2,6]];
+                    
                     obj.left = 5;
                     obj.right = 6;
+                    obj.bottom = 2;
+ 
                 case 3 %Left L Piece
                     obj.color = 4;
                     obj.locations = [[1,4],[2,4],[2,5],[2,6]];
+                    
                     obj.left = 4;
                     obj.right = 6;
+                    obj.bottom = 2;
+ 
                 case 4 %Right L Piece
                     obj.color = 2;
                     obj.locations = [[1,6],[2,4],[2,5],[2,6]];
+                    
                     obj.left = 4;
                     obj.right = 6;
+                    obj.bottom = 2;
+ 
                 case 5 %Left Zig-Zag Piece
                     obj.color = 3;
                     obj.locations = [[1,5],[1,6],[2,4],[2,5]];
+                    
                     obj.left = 4;
                     obj.right = 7;
+                    obj.bottom = 2;
+ 
                 case 6 %Right Zig-Zag Piece
                     obj.color = 4;
                     obj.locations = [[1,4],[1,5],[2,5],[2,6]];
+                    
+                    obj.left = 4;
+                    obj.right = 6;
+                    obj.bottom = 2;
+                case 7 %T-Shape Piece
+                    obj.color = 2;
+                    obj.locations = [[1,5],[2,4],[2,5],[2,6]];
+                    
+                    obj.left = 4;
+                    obj.right = 6;
+                    obj.bottom = 2;
             end
             
         end
         
-        function obj = move(obj)
-            
-            obj.locations = [];
-            
+        function obj = move(obj, dir)
+            switch dir
+                case -1
+                    if obj.left - 1 >= 1
+                        for i = 2:2:8
+                            obj.locations(i) = obj.locations(i) - 1;
+                        end
+                        obj.left = obj.left - 1;
+                        obj.right = obj.right - 1;
+                    end
+                case 0
+                    if obj.bottom + 1 <= 20
+                        for i = 1:2:8
+                            obj.locations(i) = obj.locations(i) + 1;
+                        end
+                        obj.bottom = obj.bottom + 1;
+                    end
+                case 1
+                    if obj.right + 1 <= 10
+                        for i = 2:2:8
+                            obj.locations(i) = obj.locations(i) + 1;
+                        end
+                        obj.left = obj.left + 1;
+                        obj.right = obj.right + 1;
+                    end
+            end
         end
-        
-        function [locs] = retLocs(obj)
-            locs = obj.locations;
-        end
+
+
+
     end
 end
 
--- src/main.m
+++ src/main.m
@@ -10,18 +10,19 @@ fprintf("Engineering 1181 SDP: Tetris V:0.0.1\n");
 gameScene = simpleGameEngine('../res/Tiles.png',32,32,5, [255,255,255]);
 
 %The main game board. Initialized to a blank board. (Using ints instead of doubles for better memory management).
+global gameBoard
 gameBoard = uint8(ones(20,10));
 
 %Creating a test piece.
 %gameBoard(3,5) = 3; gameBoard(3,6) = 3; gameBoard(3,7) = 3; gameBoard(4,6) = 3;
-tit = Tetromino(); titLoc = tit.retLocs();
-gameBoard(titLoc(1),titLoc(2)) = tit.color; gameBoard(titLoc(3),titLoc(4)) = tit.color; gameBoard(titLoc(5),titLoc(6)) = tit.color; gameBoard(titLoc(7),titLoc(8)) = tit.color;
+tit = Tetromino(); titLoc = tit.locations;
+gameBoard = drawTetro(tit, gameBoard);
 
 %Initializing the game scene. The scene must be drawn once before the game loop and before callback methods can be set.
 drawScene(gameScene, gameBoard);
 
 %Setting callback methods for keypress and window close events. (Handeled with functions at the bottom of the script).
-set(gameScene.my_figure, 'KeyPressFcn', @keyPressEvent);
+set(gameScene.my_figure, 'KeyPressFcn', {@keyPressEvent, tit});
 set(gameScene.my_figure, 'CloseRequestFcn', @closeCallback);
 
 %Starting the main game loop. 
@@ -35,9 +36,12 @@ while playing
 
     
     %DO GAME LOGIC HERE.
+    gameBoard = eraseTetro(tit, gameBoard);
+    tit.move(0);
+    gameBoard = drawTetro(tit, gameBoard);
 
     %This pause creates a consistent 0.1 second game loop.
-    pause(0.1-toc);
+    pause(0.3-toc);
     
     %fprintf("Time from last render loop: %.4f\n", toc);  %UNCOMMENT THIS LINE TO SEE THAT WE ARE RUNNING AT 1 LOOP EVERY 0.1 SECONDS.
 end
@@ -45,12 +49,33 @@ end
 %TODO UNCCOMENT: clear; clc;
 
 %Handels key events. (When a key is pressed this function is called).
-function keyPressEvent(~, event)
-    disp(event.Key);
+function keyPressEvent(~, event, tetro)
+    global gameBoard
+    gameBoard = eraseTetro(tetro, gameBoard);
+    if event.Key == 'a'
+        tetro.move(-1)
+    elseif event.Key == 'd'
+        tetro.move(1)
+    end
+    gameBoard = drawTetro(tetro, gameBoard);
 end
 
 %Handels window close events. (When the window is closed this function is called).
 function closeCallback(src, ~)
     assignin('base', 'playing', false);
     delete(src);
+end
+
+function gameBoard = drawTetro(tetro, gameBoard)
+    loc = tetro.locations;
+    for i = 1:2:8
+        gameBoard(loc(i),loc(i+1)) = tetro.color;
+    end
+end
+
+function gameBoard = eraseTetro(tetro, gameBoard)   
+    loc = tetro.locations;
+    for i = 1:2:8
+        gameBoard(loc(i),loc(i+1)) = 1;
+    end
 end
