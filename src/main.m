05a8139694d041ba572623894295ba5e2bde5d9b
Dannyx51 <freinds528@gmail.com>
2021-11-15 12:27:28 -0500

    updated main to work with the new behavior

--- src/main.m
+++ src/main.m
@@ -11,14 +11,14 @@ gameScene = simpleGameEngine('../res/Tiles.png',32,32,5, [255,255,255]);
 
 %The main game board. Initialized to a blank board. (Using ints instead of doubles for better memory management).
 global gameBoard
-gameBoard = uint8(ones(20,10));
+gameBoard = GameBoard();
 
 %Creating a test piece.
 %gameBoard(3,5) = 3; gameBoard(3,6) = 3; gameBoard(3,7) = 3; gameBoard(4,6) = 3;
 tetro = Tetromino(); tetroLoc = tetro.locations;
 
 %Initializing the game scene. The scene must be drawn once before the game loop and before callback methods can be set.
-drawScene(gameScene, gameBoard);
+drawScene(gameScene, gameBoard.board);
 
 %Setting callback methods for keypress and window close events. (Handeled with functions at the bottom of the script).
 set(gameScene.my_figure, 'CloseRequestFcn', @closeCallback);
@@ -34,33 +34,33 @@ while playing
     tic;
 
     %Rendering the game scene.
-    drawScene(gameScene, gameBoard);
+    drawScene(gameScene, gameBoard.board);
 
     %DO GAME LOGIC HERE.
-    gameBoard = eraseTetro(tetro, gameBoard);
-    tetro.move(0);
-    gameBoard = drawTetro(tetro, gameBoard);
-
+    gameBoard = tetro.move('d');
+    
     key_down = guidata(gameScene.my_figure);
     if(key_down)
     
         if isequal(key_down, 'a') || isequal(key_down, 'leftarrow')
-            gameBoard = eraseTetro(tetro, gameBoard);
-            tetro.move(-1);
+            gameBoard = tetro.move('l');
+        
         elseif isequal(key_down, 'd') || isequal(key_down, 'rightarrow')
-            gameBoard = eraseTetro(tetro, gameBoard);
-            tetro.move(1);
+            gameBoard = tetro.move('r');
+            
         elseif isequal(key_down, 'f1')
             tetro.MaxTicsUntilFall = tetro.MaxTicsUntilFall - 1;
             fprintf("[DEBUG]: %i\n", tetro.MaxTicsUntilFall);
+        
         elseif isequal(key_down, 'f2')
             tetro = Tetromino();
+        
         elseif isequal(key_down, 'escape')
             close(gameScene.my_figure);
             playing = false;
+        
         end
-
-        gameBoard = drawTetro(tetro, gameBoard);
+        
     end
 
     %This pause limits the fps based on the framerate variable.
@@ -74,18 +74,4 @@ end
 function closeCallback(src, ~)
     assignin('base', 'playing', false);
     delete(src);
-end
-
-function gameBoard = drawTetro(tetro, gameBoard)
-    loc = tetro.locations;
-    for i = 1:2:8
-        gameBoard(loc(i),loc(i+1)) = tetro.color;
-    end
-end
-
-function gameBoard = eraseTetro(tetro, gameBoard)   
-    loc = tetro.locations;
-    for i = 1:2:8
-        gameBoard(loc(i),loc(i+1)) = 1;
-    end
 end
