90d209b0db26fa4d1ee386f2e932dec0df6994a3
Xander C <xandercdcarroll@gmail.com>
2021-11-16 15:06:22 -0500

    Slight Fastfall Tweaks

--- src/main.m
+++ src/main.m
@@ -9,16 +9,12 @@ fprintf("Engineering 1181 SDP: Tetris V:0.0.1\n");
 %The main game scene. This will need to be drawn to every frame.
 gameScene = simpleGameEngine('../res/Tiles.png',32,32,5, [255,255,255]);
 
-%The main game board. Initialized to a blank board. (Using ints instead of doubles for better memory management).
+%The main game board that will hold a gird of pieces.
 global gameBoard
 gameBoard = GameBoard();
 
 global collided
 collided = false;
-
-%Creating a test piece.
-%gameBoard(3,5) = 3; gameBoard(3,6) = 3; gameBoard(3,7) = 3; gameBoard(4,6) = 3;
-tetro = Tetromino(); tetroLoc = tetro.locations;
 
 %Initializing the game scene. The scene must be drawn once before the game loop and before callback methods can be set.
 drawScene(gameScene, gameBoard.board);
@@ -35,17 +31,15 @@ framerate = 10;
 %Piece Speed
 pieceSpeed = 5;
 
-%Was key pressed variable
-wasKeyJustPressed = false;
+%Creating the first piece.
+tetro = Tetromino(); tetroLoc = tetro.locations;
+
+%Used to create an if branch that will execute only a single time on a keypress.
+wasKeyJustPressed = 0;
 
 playing = true;
 while playing
     tic;
-    
-    if collided
-        tetro = tetro.copytetro(Tetromino());
-        collided = false;
-    end
 
     %Rendering the game scene.
     drawScene(gameScene, gameBoard.board);
@@ -53,50 +47,63 @@ while playing
     %Moving the tetromino down.
     [gameBoard, collided] = tetro.move('d', gameBoard);
 
+    if (collided)
+        tetro = Tetromino();
+        collided = false;
+    end
+
     %Handling user input.
     key_down = guidata(gameScene.my_figure);
     if(key_down)
-        wasKeyJustPressed = true;
+        wasKeyJustPressed = key_down;
+
+        %Left piece movement
         if isequal(key_down, 'a') || isequal(key_down, 'leftarrow')
             [gameBoard, collided] = tetro.move('l', gameBoard);
         
+        %Right piece movement
         elseif isequal(key_down, 'd') || isequal(key_down, 'rightarrow')
             [gameBoard, collided] = tetro.move('r', gameBoard);
         
+        %Start piece fast fall
         elseif isequal(key_down, 's') || isequal(key_down, 'downarrow')
             tetro.maxTicsUntilFall = 0;
             tetro.ticsUntilFall = 0;   
         
+        %Escape key to close game
         elseif isequal(key_down, 'escape')
             close(gameScene.my_figure);
             playing = false;
         
-        %Temporary key input used for debuging.
+        %TODO REMOVE: Temporary key input used for debuging.
         elseif isequal(key_down, 'f1')
             pieceSpeed = pieceSpeed - 1;
             tetro.maxTicsUntilFall = pieceSpeed;
             fprintf("[DEBUG]: New Piece Speed = %i\n", pieceSpeed);
-        elseif isequal(key_down, 'f2')
-            tetro = Tetromino();
-            fprintf("[DEBUG]: Piece Created\n");
         end
         
     else
-        if wasKeyJustPressed
+
+        %When the down key is released stop fast fall.
+        if isequal(wasKeyJustPressed, 's') || isequal(wasKeyJustPressed, 'downarrow')
             tetro.maxTicsUntilFall = pieceSpeed;
             tetro.ticsUntilFall = pieceSpeed;
-            wasKeyJustPressed = false;
+        elseif isequal(wasKeyJustPressed, 'f2')
+            tetro = Tetromino();
+            fprintf("[DEBUG]: Piece Created\n");
         end
+    
+        wasKeyJustPressed = 0;
     end
 
     %This pause limits the fps based on the framerate variable.
     pause(1/framerate-toc);
-    %fprintf("Framerate: %f\n", 1/toc); %Unccoment this line to see framerate.
+    %fprintf("Framerate: %f\n", 1/toc); %TODO REMOVE: Unccoment this line to see framerate.
 end
 
 %TODO UNCOMMENT: clear; clc;
 
-%Handels window close events. (When the window is closed this function is called).
+%Handels window close events. (When the window close button is pressed this function is called).
 function closeCallback(src, ~)
     assignin('base', 'playing', false);
     delete(src);
