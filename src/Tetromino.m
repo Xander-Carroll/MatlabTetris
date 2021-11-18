10342dddd97541dd63696d9cf51c7e6a792d01bd
Dhaanish K <freinds528@gmail.com>
2021-11-18 02:02:31 -0500

    ROTATE IS FINISHED WE HAVE A WORKING GAME BOYS LETS GOOOOOOOOOOOOOOOOO

--- src/Tetromino.m
+++ src/Tetromino.m
@@ -1,14 +1,17 @@
 classdef Tetromino < handle
+
     properties
         %Used to control tetromino appearance/movement.
         type
         color
+
+        %Used to keep the location of the piece and keeps it from moving out of the board.
         locations
         left
         right
-
         top
         bottom
+        pivot
 
         %Used to control tetromino speed.
         maxTicsUntilFall = 5;
@@ -16,20 +19,23 @@ classdef Tetromino < handle
     end
 
     methods
+        %Picks a random piece from the list of 7 and initializes the class properities based on the piece picked.
         function obj = Tetromino()
 
             %Used to pick a random tetromino piece.
             obj.type = randi(7);
-            
+            % obj.type = 1 + randi(6);
+
             switch obj.type
                 case 1 %Line Piece
-                    obj.color = 4;
+                    obj.color = 8;
                     obj.locations = [[1,4],[1,5],[1,6],[1,7]];
                     
                     obj.left = 4;
                     obj.right = 7;
                     obj.top = 1;
                     obj.bottom = 1;
+                    obj.pivot = [1.5, 5.5];
  
                 case 2 %Square Piece
                     obj.color = 6;
@@ -39,6 +45,7 @@ classdef Tetromino < handle
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [1.5,5.5];
  
                 case 3 %Left L Piece
                     obj.color = 3;
@@ -48,15 +55,17 @@ classdef Tetromino < handle
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [2,5];
  
                 case 4 %Right L Piece
-                    obj.color = 3;
+                    obj.color = 4;
                     obj.locations = [[1,6],[2,4],[2,5],[2,6]];
                     
                     obj.left = 4;
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [2,5];
  
                 case 5 %Left Zig-Zag Piece
                     obj.color = 2;
@@ -66,6 +75,7 @@ classdef Tetromino < handle
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [2,5];
  
                 case 6 %Right Zig-Zag Piece
                     obj.color = 5;
@@ -75,6 +85,7 @@ classdef Tetromino < handle
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [2,5];
 
                 case 7 %T-Shape Piece
                     obj.color = 7;
@@ -84,6 +95,7 @@ classdef Tetromino < handle
                     obj.right = 6;
                     obj.top = 1;
                     obj.bottom = 2;
+                    obj.pivot = [2,5];
 
             end
             
@@ -103,6 +115,8 @@ classdef Tetromino < handle
                         end
                         obj.left = obj.left - 1;
                         obj.right = obj.right - 1;
+                        
+                        obj.pivot(2) = obj.pivot(2) - 1;
                     end
                 case 'd' % move down
                     if obj.ticsUntilFall > 0
@@ -112,12 +126,14 @@ classdef Tetromino < handle
                         obj.ticsUntilFall = obj.maxTicsUntilFall;
                     end
                     
-                    if obj.bottom + 1 <= 20 && ~collided
+                    if obj.bottom + 1 <= 23 && ~collided
                         for i = 1:2:8
                             obj.locations(i) = obj.locations(i) + 1;
                         end
                         obj.top = obj.top + 1;
                         obj.bottom = obj.bottom + 1;
+
+                        obj.pivot(1) = obj.pivot(1) + 1;
                     end
                 case 'r' % move right
                     if obj.right + 1 <= 10 && ~collided
@@ -126,6 +142,8 @@ classdef Tetromino < handle
                         end
                         obj.left = obj.left + 1;
                         obj.right = obj.right + 1;
+
+                        obj.pivot(2) = obj.pivot(2) + 1;
                     end
             end
 
@@ -140,11 +158,83 @@ classdef Tetromino < handle
             obj.right = tetro.right;
             obj.top = tetro.top;
             obj.bottom = tetro.bottom;
-    
+            obj.pivot = tetro.pivot;
+
             obj.maxTicsUntilFall = tetro.maxTicsUntilFall;
             obj.ticsUntilFall = tetro.ticsUntilFall;
         end
 
+        function gameboard = rotate(obj, gameboard)
+            dir = 1;
+                
+            pre = Tetromino().copytetro(obj);
+            loc = obj.locations;
+            board = gameboard.board;
+
+            % set the rotation point based on tetromino type
+            midx = obj.pivot(2);
+            midy = obj.pivot(1);
+            
+            for i = 1:2:8 % rotate the pieces 90 counter clockwise
+                temp = loc(i);
+                loc(i) = loc(i + 1) - midx + midy;
+                loc(i + 1) = -temp + midy + midx;
+            end
+            
+            %adjust for if the piece has moved out of bounds
+            xs = loc(2:2:8);
+            ys = loc(1:2:8);
+
+            if min(xs) < 1 % adjusting for left bounds
+                dx = abs(1 - min(xs));
+            elseif max(xs) > 10 % adjusting for right bounds
+                dx = 10 - max(xs);
+            else
+                dx = 0;
+            end
+
+            if min(ys) < 1 % adjusting for ceiling
+                dy = abs(1 - min(ys));
+            elseif max(ys) > 24 % adjusting for floor
+                dy = 24 - max(ys);
+            else
+                dy = 0;
+            end
+
+            for i = 1:2:8
+                loc(i) = loc(i) + dy;
+                loc(i + 1) = loc(i + 1) + dx;
+            end
+
+            % check to see if the piece has rotated where it can't be
+            for i = 1:2:8
+                cont = false;
+                for x = 1:2:8
+                    if (loc(i) == loc(x) && loc(i + 1) == loc(x + 1))
+                        cont = true; break;
+                    elseif (loc(i) == obj.locations(x) && loc(i + 1) == obj.locations(x + 1))
+                        cont = true; break;
+                    end
+                end
+                if cont; continue; end
+                
+                if board(loc(i), loc(i + 1)) ~= 1
+                    return;
+                end
+            end
+            
+            obj.locations = loc;
+
+            xs = obj.locations(2:2:8);
+            ys = obj.locations(1:2:8);
+
+            obj.top = min(ys);
+            obj.bottom = max(ys);
+            obj.left = min(xs);
+            obj.right = max(xs);
+
+            gameboard = gameboard.update(pre, obj);
+        end
     end
 end
 
@@ -158,7 +248,7 @@ function isCollide = checkCollide(obj, gameboard, dir)
         isCollide = false;
         return;
     end
-    if (max(ys) == 20 && isequal(dir, 'd')) 
+    if (max(ys) == 23 && isequal(dir, 'd')) 
         isCollide = true;
         return;
     end
--- src/main.m
+++ src/main.m
@@ -3,97 +3,127 @@
 %Developed by Danny, Matt, and Xander. Engineering 1181 SDP.
 
 clear; clc;
-fprintf("Engineering 1181 SDP: Tetris V:0.0.1\n");
+fprintf("Engineering 1181 SDP: Tetris V:0.0.2\n");
 %%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%%
 
+%The main game framerate target. (If you set the framerate too high the game won't close).
+framerate = 15; 
+
 %The main game scene. This will need to be drawn to every frame.
-gameScene = simpleGameEngine('../res/Tiles.png',32,32,5, [255,255,255]);
+gameScene = simpleGameEngine('../res/Tiles.png',32,32,1,[255,255,255]);
 
 %The main game board that will hold a gird of pieces.
-global gameBoard
 gameBoard = GameBoard();
+gameBoard = gameBoard.generateTitleBoard();
 
-global collided
+%Used to determine if a piece has landed (and therefore a new piece should be made).
 collided = false;
+collideTimerMax = 5;
+collideTimer = 5;
 
 %Initializing the game scene. The scene must be drawn once before the game loop and before callback methods can be set.
-drawScene(gameScene, gameBoard.board);
+drawScene(gameScene, gameBoard.getVisibleBoard());
 
-%Setting callback methods for keypress and window close events. (Handeled with functions at the bottom of the script).
+%Setting a callback method for the window close event. (Handeled with function at the bottom of the script).
 set(gameScene.my_figure, 'CloseRequestFcn', @closeCallback);
 
-%Starting the main game loop. 
-%playing will become false when the game window is closed.
-
-%The main game framerate target. (If you set the framerate too high the game won't close).
-framerate = 10; 
-
-%Piece Speed
+%The speed at which the pieces fall. A smaller speed make faster pieces.
 pieceSpeed = 5;
 
-%Creating the first piece.
+%Creating the first piece. Once this piece lands a new piece is made.
 tetro = Tetromino(); tetroLoc = tetro.locations;
 
-%Used to create an if branch that will execute only a single time on a keypress.
-wasKeyJustPressed = 0;
+wasDownJustPressed = false;
 
+%Starting the main game loop. 
+%playing will become false when the game window is closed (Or escape is pressed).
 playing = true;
+inTitleScreen = true;
 while playing
     tic;
 
     %Rendering the game scene.
-    drawScene(gameScene, gameBoard.board);
+    drawScene(gameScene, gameBoard.getVisibleBoard());
 
-    %Moving the tetromino down.
-    [gameBoard, collided] = tetro.move('d', gameBoard);
-
-    if (collided)
-        tetro = Tetromino();
-        collided = false;
-    end
-
-    %Handling user input.
-    key_down = guidata(gameScene.my_figure);
-    if(key_down)
-        wasKeyJustPressed = key_down;
-
-        %Left piece movement
-        if isequal(key_down, 'a') || isequal(key_down, 'leftarrow')
-            [gameBoard, collided] = tetro.move('l', gameBoard);
-        
-        %Right piece movement
-        elseif isequal(key_down, 'd') || isequal(key_down, 'rightarrow')
-            [gameBoard, collided] = tetro.move('r', gameBoard);
-        
-        %Start piece fast fall
-        elseif isequal(key_down, 's') || isequal(key_down, 'downarrow')
-            tetro.maxTicsUntilFall = 0;
-            tetro.ticsUntilFall = 0;   
-        
-        %Escape key to close game
-        elseif isequal(key_down, 'escape')
-            close(gameScene.my_figure);
-            playing = false;
-        
-        %TODO REMOVE: Temporary key input used for debuging.
-        elseif isequal(key_down, 'f1')
-            pieceSpeed = pieceSpeed - 1;
-            tetro.maxTicsUntilFall = pieceSpeed;
-            fprintf("[DEBUG]: New Piece Speed = %i\n", pieceSpeed);
+    if (inTitleScreen) 
+        [y,x] = getMouseInput(gameScene);
+        if (y <= 10) %singleplayer
+            inTitleScreen = false;
+            gameBoard.board = uint8(ones(23,10));
+        else %multiplayer
+            inTitleScreen = false;
         end
-        
     else
+    
+        %Moving the tetromino down.
+        [gameBoard, collided] = tetro.move('d', gameBoard);
+    
+        %Handling user input.
+        key_down = guidata(gameScene.my_figure);
+        if(key_down)
+            %Left piece movement
+            if isequal(key_down, 'a') || isequal(key_down, 'leftarrow')
+                [gameBoard, ~] = tetro.move('l', gameBoard);
+            
+            %Right piece movement
+            elseif isequal(key_down, 'd') || isequal(key_down, 'rightarrow')
+                [gameBoard, ~] = tetro.move('r', gameBoard);
+            
+            elseif isequal(key_down, 'w')
+                gameBoard = tetro.rotate(gameBoard);
 
-        %When the down key is released stop fast fall.
-        if isequal(wasKeyJustPressed, 's') || isequal(wasKeyJustPressed, 'downarrow')
-            tetro.maxTicsUntilFall = pieceSpeed;
-            tetro.ticsUntilFall = pieceSpeed;
-        elseif isequal(wasKeyJustPressed, 'f2')
-            tetro = Tetromino();
-            fprintf("[DEBUG]: Piece Created\n");
+            %Start piece fast fall
+            elseif isequal(key_down, 's') || isequal(key_down, 'downarrow')
+                tetro.maxTicsUntilFall = 0;
+                tetro.ticsUntilFall = 0;
+                wasDownJustPressed = true;
+            end
+            
+            %Escape key to close game
+            if isequal(key_down, 'escape')
+                close(gameScene.my_figure);
+                playing = false;
+            
+            %TODO REMOVE: Temporary key input used for debuging.
+            elseif isequal(key_down, 'f1')
+                pieceSpeed = pieceSpeed - 1;
+                tetro.maxTicsUntilFall = pieceSpeed;
+                fprintf("[DEBUG]: New Piece Speed = %i\n", pieceSpeed);
+            end
+        else
+            if (wasDownJustPressed)
+                tetro.maxTicsUntilFall = pieceSpeed;
+                tetro.ticsUntilFall = pieceSpeed;
+                wasDownJustPressed = false;
+            end
         end
     
-        wasKeyJustPressed = 0;
+        %Once a piece has landed it is determined if the player has lost, if any lines have been cleared, and creates a new tetro.
+        if (collided)
+            if(collideTimer > 0)
+                collideTimer = collideTimer - 1;
+            else
+            
+                if (gameBoard.isGameOver())
+                    inTitleScreen = true;
+                    gameBoard = gameBoard.generateTitleBoard();
+                else
+                    
+                    [~, collided] = tetro.move('d',gameBoard);
+                    if (collided)
+                        collideTimer = collideTimerMax;
+                        tetro = Tetromino();
+                        tetro.maxTicsUntilFall = pieceSpeed;
+                    end
+
+                    collided = false;
+    
+                    gameBoard = gameBoard.clearCompleteRows();
+                end
+    
+            end
+    
+        end
     end
 
     %This pause limits the fps based on the framerate variable.
