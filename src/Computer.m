8147685c815f1bafb7eb9970d2712fbc9a08f0b3
XanderC <xandercdcarroll@gmail.com>
2021-12-02 12:02:50 -0500

    New AI

--- src/Computer.m
+++ src/Computer.m
@@ -2,70 +2,65 @@ function [tetro,final] = Computer(tetro,orig)
 %COMPUTER Summary of this function goes here
 %   Detailed explanation goes here
     
-    bestMove = [randi(8) - 5, randi(4) - 1];
+%     bestMove = [randi(8) - 5, randi(4) - 1];
+      
+    bestMove = [0,0];
+    copied = Tetromino().copytetro(tetro);
+    for t = 1:4
+        left = Tetromino().copytetro(copied);
+        right = Tetromino().copytetro(copied);
+        
+        prevLeft = left.left;
+        hitWall = false;
+        numLeft = 0;
+        bestHeight = 1;
+        while ~hitWall
+            gameboard = orig;
+            [gameboard, ~] = left.move('l', gameboard);
+            numLeft = numLeft + 1;
+            curLeft = left.left;
+            
+            cleft = Tetromino().copytetro(left);
+            while ~gameboard.collided
+                [gameboard, gameboard.collided] = cleft.move('d', gameboard);
+            end
+            
+            curHeight = cleft.top;
+            if curHeight > bestHeight
+                bestMove = [-numLeft, t];
+                bestHeight = curHeight;
+            end
 
-%     bestHeight = 0;
-%     copied = Tetromino().copytetro(tetro);
-%     for t = 1:4
-%         left = Tetromino().copytetro(copied);
-%         right = Tetromino().copytetro(copied);
-%         
-%         prevLeft = left.left;
-%         hitWall = false;
-%         numLeft = 0;
-%         while ~hitWall
-%             gameboard = orig;
-%             [gameboard, ~] = left.move('l', gameboard);
-%             numLeft = numLeft + 1;
-%             curLeft = left.left;
-%             
-%             cleft = Tetromino().copytetro(left);
-%             while ~gameboard.collided
-%                 [gameboard, gameboard.collided] = cleft.move('d', gameboard);
-%             end
-% 
-%             for i = 23:-1:1
-%                 disp(gameboard.board(i,:))
-%                 if sum(gameboard.board(i,:)) == 10
-%                     if i > bestHeight
-%                         bestHeight = i;
-%                         bestMove = [numLeft, t];
-%                         break;
-%                     end
-%                 end
-%             end
-%             hitWall = curLeft == prevLeft;
-%             prevLeft = curLeft;
-%         end
-% 
-%         prevRight = right.right;
-%         hitWall = false;
-%         numRight = 0;
-%         while ~hitWall
-%             gameboard = orig;
-%             [gameboard, ~] = right.move('r', gameboard);
-%             numRight = numRight + 1;
-%             curRight = right.right;
-%             
-%             cright = Tetromino().copytetro(right);
-%             while ~gameboard.collided
-%                 [gameboard, gameboard.collided] = cright.move('d', gameboard);
-%             end
-% 
-%             for i = 23:-1:1
-%                 if sum(gameboard.board(i,:)) == 0
-%                     if i > bestHeight
-%                         bestHeight = i;
-%                         bestMove = [-numLeft, t];
-%                     end
-%                 end
-%             end
-%             hitWall = curRight == prevRight;
-%             prevRight = curRight;
-%         end
-% 
-%         gameboard = copied.rotate(gameboard);
-%     end
+            hitWall = curLeft == prevLeft;
+            prevLeft = curLeft;
+        end
+
+        prevRight = right.right;
+        hitWall = false;
+        numRight = 0;
+        while ~hitWall
+            gameboard = orig;
+            [gameboard, ~] = right.move('r', gameboard);
+            numRight = numRight + 1;
+            curRight = right.right;
+            
+            cright = Tetromino().copytetro(right);
+            while ~gameboard.collided
+                [gameboard, gameboard.collided] = cright.move('d', gameboard);
+            end
+
+            curHeight = cright.top;
+            if curHeight > bestHeight
+                bestMove = [numRight, t];
+                bestHeight = curHeight;
+            end
+
+            hitWall = curRight == prevRight;
+            prevRight = curRight;
+        end
+
+        gameboard = copied.rotate(gameboard);
+    end
      
     final = orig;
     if bestMove(1) < 0 && bestMove(1) ~= 0
@@ -77,6 +72,7 @@ function [tetro,final] = Computer(tetro,orig)
             [final, ~] = tetro.move('r', final);
         end
     end
+
     for i = 1:bestMove(2)
         final = tetro.rotate(final);
     end
